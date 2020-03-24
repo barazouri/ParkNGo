3a1d070023801813fa4a1ecf3da1158fd0bea375
Bar Azouri <barazouri24@gmail.com>
2020-03-24 18:48:22 +0200

    bash for camera code

---
+++ camera.sh
@@ -0,0 +1,3 @@
+python3 -m venv work
+source LicencePlateRecognition/openalpr/src/bindings/python/work/bin/activate
+python3 LicencePlateRecognition/openalpr/src/bindings/python/cameraStream.py
--- server/controller/lisencePlateRecognition.js
+++ server/controller/lisencePlateRecognition.js
@@ -1,9 +1,10 @@
 module.exports = {
     licensePlateUpdate(req, res) {
         try {
-            let {licenseResults} = req.body
-            licenseResults = JSON.parse(licenseResults)
-            let licensePlateNumber = licenseResults[0].results[0].plate
+            let {plateNumber} = req.body
+            console.log(plateNumber)
+            plateNumber = JSON.parse(plateNumber)
+            let licensePlateNumber = plateNumber[0].results[0].plate
             console.log(licensePlateNumber)
             return res.json(licensePlateNumber);
          } catch (err) { console.error(err);return res.json(err); }
