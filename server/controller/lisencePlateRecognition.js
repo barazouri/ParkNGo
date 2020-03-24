07345279b3188208a5b310fd09c18432b3edb04e
Bar Azouri <barazouri24@gmail.com>
2020-03-24 20:33:10 +0200

    licensePlateUpdate node rest api

--- server/controller/lisencePlateRecognition.js
+++ server/controller/lisencePlateRecognition.js
@@ -1,12 +1,44 @@
+const Profiles = require('../models/profiles');
+
 module.exports = {
-    licensePlateUpdate(req, res) {
+    async licensePlateUpdate(req, res) {
         try {
-            let {plateNumber} = req.body
-            console.log(plateNumber)
-            plateNumber = JSON.parse(plateNumber)
-            let licensePlateNumber = plateNumber[0].results[0].plate
-            console.log(licensePlateNumber)
-            return res.json(licensePlateNumber);
+            let {plateNumber = null, profileId = null, parkingId = null} = req.body
+            const userFound = await Profiles.find({ profileId: profileId });
+            if (!userFound.length) {
+                console.log("A profile with that gmail account isn't exist");
+                return res.json("A profile with that gmail account isn't exist");
+            }
+            else {
+             await Profiles.updateOne(
+                    {"profileId": profileId, "parkingSpots.parkingId": parkingId},
+                    { $set: {'parkingSpots.$.availability': plateNumber} }
+                )
+                }
+            return res.json(plateNumber);
          } catch (err) { console.error(err);return res.json(err); }
     }
 }
+
+
+// async editSpecificParking(req, res, next) {
+//     try {
+//        const { parkingId = null, address = null, policy = null, parkingSize = null, price = null, windowsOfTime = null } = req.body;
+//        const parkingIDFound = await Profiles.find({ parkingSpots: {$elemMatch: {parkingId }}});
+//        if (!parkingIDFound.length) {
+//           console.log("A parking spot with that ID does not exist");
+//           return res.json("A parking spot with that ID does not exist");
+//        }
+//        else {
+//           let parkingSpot = { "parkingId": parkingId,"address": address, "policy": policy, "parkingSize": parkingSize, "price": price, "windowsOfTime": windowsOfTime }
+//           await Profiles.updateOne(
+//              { parkingSpots: {$elemMatch: {parkingId: parkingId }} },
+//              { $set: { parkingSpots: parkingSpot } }
+//           )
+//           // console.log(`${email}'s profile updated: \n driverLicensePlate -> ${driverLicensePlate} \n driverCarSize -> ${driverCarSize}`);
+//           // res.json(`${email}'s profile updated: driverLicensePlate -> ${driverLicensePlate}, driverCarSize -> ${driverCarSize}`);
+//        }
+
+//     } catch (err) { console.error(err);return res.json(err); };
+
+//  },
