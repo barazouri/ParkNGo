6e8f4a31af579de50227cb904009ba4128f1757a
Guygolpur <guygolpur@se.shenkar.ac.il>
2020-04-01 16:59:25 +0300

    search with futureReservation works+ booking functions

--- server/controller/search.ctl.js
+++ server/controller/search.ctl.js
@@ -1,5 +1,5 @@
 const Profiles = require('../models/profiles');
-var distance = require('google-distance');
+var AvailabilitySchedule = require('availability-schedule');
 
 module.exports = {
    /**Get without params */
@@ -155,38 +155,57 @@ async searchParkingSpotByLocation(req, res, next) {
            }
          })
       })
+
       allParkingSpots.map(parkingSpot => {
-         console.log("parkingSpot.futureReservations: ");
-         console.log("length: " + parkingSpot.futureReservations.length + ' parkingId: ' + parkingSpot.parkingId)
-         if(parkingSpot.futureReservations.length <= 0)
-         {
-            resultParkingSpots.push(parkingSpot);
-            console.log("enter first: ");
-            console.log(resultParkingSpots);
-         }
+         var schedule = new AvailabilitySchedule(parkingSpot.windowsOfTime.AvailablefromTime, parkingSpot.windowsOfTime.AvailableUntilTime); // Second week of Jan 2017
+         parkingSpot.windowsOfTime.map(window => {
+            // schedule.addWeeklyRecurringAvailability('2017-01-04T09:00:00Z', '2017-01-04T17:00:00Z', [1, 2, 3, 4, 5]); // Mon-Fri 9am-5pm UTC, starting on Wed Jan 4th
+            schedule.addAvailability(window.AvailablefromTime, window.AvailableUntilTime); // Sat Jan 14 12pm-3pm UTC
+ 
+            schedule.getAvailabilities('+0100');
+         })
          parkingSpot.futureReservations.map(future => {
-            let futureFromMFrom = future.requireToDate.getTime() - fromTimeDate.getTime()//
-            let futureFromMUntil = future.requireToDate.getTime() - untilTimeDate.getTime()//
-            let futureUntilMFrom = future.requireUntilDate.getTime() - fromTimeDate.getTime()//
-            let futureUntilMUntil = future.requireUntilDate.getTime() - untilTimeDate.getTime()//
-         if((futureFromMFrom > 0)&&(futureFromMUntil > 0))
+            schedule.removeAvailability (future.requireToDate, future.requireUntilDate);
+         })
+         if(schedule.isAvailable(fromTime,untilTime))
          {
-            console.log("2");
-            console.log("futureFromMUntil: " + futureFromMUntil)
-            resultParkingSpots.push(parkingSpot);
-            console.log("enter second: ");
-            console.log(resultParkingSpots);
-         }
-         if((futureUntilMFrom < 0)&&(futureUntilMUntil < 0))
-         {
-            console.log("3");
-            resultParkingSpots.push(parkingSpot);
-            console.log("enter third: ");
-            // console.log(resultParkingSpots);
+            resultParkingSpots.push(parkingSpot)
          }
       })
-   })
-      // console.log(resultParkingSpots);
+
+
+   //    allParkingSpots.map(parkingSpot => {
+   //       console.log("parkingSpot.futureReservations: ");
+   //       console.log("length: " + parkingSpot.futureReservations.length + ' parkingId: ' + parkingSpot.parkingId)
+   //       if(parkingSpot.futureReservations.length <= 0)
+   //       {
+   //          resultParkingSpots.push(parkingSpot);
+   //          console.log("enter first: ");
+   //          console.log(resultParkingSpots);
+   //       }
+   //       parkingSpot.futureReservations.map(future => {
+   //          let futureFromMFrom = future.requireToDate.getTime() - fromTimeDate.getTime()//
+   //          let futureFromMUntil = future.requireToDate.getTime() - untilTimeDate.getTime()//
+   //          let futureUntilMFrom = future.requireUntilDate.getTime() - fromTimeDate.getTime()//
+   //          let futureUntilMUntil = future.requireUntilDate.getTime() - untilTimeDate.getTime()//
+   //       if((futureFromMFrom > 0)&&(futureFromMUntil > 0))
+   //       {
+   //          console.log("2");
+   //          console.log("futureFromMUntil: " + futureFromMUntil)
+   //          resultParkingSpots.push(parkingSpot);
+   //          console.log("enter second: ");
+   //          console.log(resultParkingSpots);
+   //       }
+   //       if((futureUntilMFrom < 0)&&(futureUntilMUntil < 0))
+   //       {
+   //          console.log("3");
+   //          resultParkingSpots.push(parkingSpot);
+   //          console.log("enter third: ");
+   //          // console.log(resultParkingSpots);
+   //       }
+   //    })
+   // })
+      console.log(resultParkingSpots);
       res.json(resultParkingSpots);
    } catch (err) 
    { console.error(err);
@@ -207,6 +226,8 @@ async searchByLocationAndPriceAndSizeByCurrentTime(req, res, next) {
        return res.json("A parking spot with that address does not exist");
     }
       const allParkingSpots = [];
+      const resultParkingSpots = [];
+
       result.map(profile => {
        profile.parkingSpots.map(parkingSpot => {
 
@@ -239,8 +260,29 @@ async searchByLocationAndPriceAndSizeByCurrentTime(req, res, next) {
            }
          })
       })
-      console.log(allParkingSpots);
-      res.json(allParkingSpots);
+
+      const start = new Date().toISOString()
+
+      allParkingSpots.map(parkingSpot => {
+         var schedule = new AvailabilitySchedule(parkingSpot.windowsOfTime.AvailablefromTime, parkingSpot.windowsOfTime.AvailableUntilTime); // Second week of Jan 2017
+         parkingSpot.windowsOfTime.map(window => {
+            // schedule.addWeeklyRecurringAvailability('2017-01-04T09:00:00Z', '2017-01-04T17:00:00Z', [1, 2, 3, 4, 5]); // Mon-Fri 9am-5pm UTC, starting on Wed Jan 4th
+            schedule.addAvailability(window.AvailablefromTime, window.AvailableUntilTime); // Sat Jan 14 12pm-3pm UTC
+ 
+            schedule.getAvailabilities('+0100');
+         })
+         parkingSpot.futureReservations.map(future => {
+            schedule.removeAvailability (future.requireToDate, future.requireUntilDate);
+         })
+         console.log(start)
+         if(schedule.isAvailable(start,untilTime))
+         {
+            resultParkingSpots.push(parkingSpot)
+         }
+
+      })
+      console.log(resultParkingSpots);
+      res.json(resultParkingSpots);
    } catch (err) 
    { console.error(err);
       return res.json(err); 
