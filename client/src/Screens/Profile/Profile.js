af589cd987cc76088028b5a9b0b2db288915b35a
Bar Azouri <barazouri24@gmail.com>
2020-04-05 18:39:20 +0300

    Profile change mode button

--- client/src/Screens/Profile/Profile.js
+++ client/src/Screens/Profile/Profile.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native'
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 })
 
 class Profile extends React.Component {
-
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Profile</Text>
-            </View>
-        );
+  constructor (props) {
+    super(props)
+    this.changeUsermode = this.changeUsermode.bind(this)
+    this.state = {
+      changeUserTo: 'host'
     }
+  }
+  changeUsermode () {
+    const { route } = this.props
+    if (this.state.changeUserTo == 'driver') {
+      this.setState(
+        { changeUserTo: 'host' },
+        route.params.changeKindUser(this.state.changeUserTo),
+      )
+    } else {
+      this.setState(
+        { changeUserTo: 'driver' },
+        route.params.changeKindUser(this.state.changeUserTo)
+      )
+    }
+  }
+  render () {
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={this.changeUsermode}
+          title={`change to ${this.state.changeUserTo}`}
+        />
+        {/* <Text>hello</Text> */}
+      </View>
+    )
+  }
 }
-export default Profile;
+export default Profile
+
