8bd09c51e59fcda7ade8e22ef7e63fef995472a6
Bar Azouri <barazouri24@gmail.com>
2020-04-25 13:43:32 +0300

    change logo location

--- client/src/Components/HeaderLogo/HeaderLogo.js
+++ client/src/Components/HeaderLogo/HeaderLogo.js
@@ -2,21 +2,20 @@ import React from 'react'
 import { View, Image } from 'react-native'
 
 const HeaderLogo = props => {
-
-
   return (
-    <View style={{ backgroundColor:'white'}}>
-    <Image
-      style={{
-        width: 60,
-        height: 60,
-        marginTop: 35,
-        marginLeft: 28,
-        marginBottom:10
-      }}
-      source={require('../../../images/logo.png')}
-    />
-  </View>
+    <View style={{ backgroundColor: 'white' }}>
+      <Image
+        style={{
+          width: 60,
+          height: 60,
+          marginTop: 35,
+          marginRight: 28,
+          marginBottom: 10,
+          alignSelf: 'flex-end'
+        }}
+        source={require('../../../images/logo.png')}
+      />
+    </View>
   )
 }
 export default HeaderLogo
