bfb83c3d0b328a7c00fdd92f289e3b426ddd8ca7
Guygolpur <guygolpur@se.shenkar.ac.il>
2020-06-27 16:41:18 +0300

    host Waiting for approval- approval works, need to fix Decline..

--- client/src/Host/Components/HostShowCalendarSting/hostShowCalendarSting.js
+++ client/src/Host/Components/HostShowCalendarSting/hostShowCalendarSting.js
@@ -4,7 +4,8 @@ import SafeAreaView from 'react-native-safe-area-view'
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 import { Ionicons } from '@expo/vector-icons' // 6.2.2
 import StarRating from 'react-native-star-rating'
-
+import VerifyPopUp from '../VerifyPopUp/verifyPopUp'
+import config from '../../../../config/config.json'
 
 const { height } = Dimensions.get('window');
 
@@ -41,15 +42,70 @@ class HostShowCalendarSting extends React.Component {
         super(props)
         this.state = {
             parkingSpot: props.route.params.parkingSpot,
-            dataToShow: props.route.params.dataToShow
-
+            dataToShow: props.route.params.dataToShow,
+            dialogVisibleAnswerParking: false,
+            saveFeedBackVisible: false,
+            setUser: {}
         }
         this.onContentSizeChange = this.onContentSizeChange.bind(this)
+        this.closePopUp = this.closePopUp.bind(this)
+        this.getUrlurlApproveOrDeclineForApi = this.getUrlurlApproveOrDeclineForApi.bind(this)
+        this.saveFeedBack = this.saveFeedBack.bind(this)
+        this.setUser = this.setUser.bind(this)
     }
 
 
     onContentSizeChange(contentWidth, contentHeight) {
         this.setState({ screenHeight: contentHeight })
+    }
+
+    closePopUp(childData, childDataAnswer) {
+        const { navigation } = this.props
+
+        this.setState({
+            dialogVisibleAnswerParking: childData,
+            saveFeedBackVisible: childData,
+        })
+
+        if (childDataAnswer) {
+            console.log('aprroved the request')
+            let urlApproveOrDecline = this.getUrlurlApproveOrDeclineForApi()
+            console.log('urlApproveOrDecline: ', urlApproveOrDecline)
+
+            console.log('this.state.setUser.requireToDate: ', this.state.setUser.requireToDate)
+            let url = `${urlApproveOrDecline}`
+            fetch(`${url}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: `email=${this.state.setUser.bookedBy}&parkingSpotID=${this.state.parkingSpot.parkingId}&hostID=${1}&requireToDate=${this.state.setUser.requireToDate}&requireUntilDate=${this.state.setUser.requireUntilDate}&answer=${true}` // <-- Post parameters
+            })  //check in the driver order spot the isAutomatic condition... now its=TRUE and need to be change dinamically acording to windowsOfTime.isAutomatic
+                .then(res => res.json())
+                .catch(err => new Error(err))
+            this.saveFeedBack()
+
+
+            navigation.navigate('HostParkingSpotCalendar')
+        }
+        else {  // need to add like above but with answer=${false}
+            console.log('NOT aprroved')
+        }
+
+    }
+
+    saveFeedBack() {
+        this.setState({ dialogVisibleAnswerParking: true });
+    }
+
+    getUrlurlApproveOrDeclineForApi() {
+        return config.API + `/approveOrDeclineReq`
+    }
+
+    setUser(hostWaitingQueue) {
+        this.setState({ setUser: hostWaitingQueue })
+        console.log('setUser: ', this.state.setUser)
+        this.saveFeedBack()
     }
 
     render() {
@@ -120,15 +176,20 @@ class HostShowCalendarSting extends React.Component {
                         <Text style={styles.waitingTitle}>Waiting For Approval</Text>
                         {this.state.parkingSpot.hostWaitingQueue.map((hostWaitingQueue, i) => {
                             return (
-                                <Card>
-                                    {
-                                        <View key={i} style={styles.user}>
-                                            <Text style={styles.name}>Requested by: {hostWaitingQueue.bookedBy}</Text>
-                                            <Text style={styles.name}>Require to: {hostWaitingQueue.requireToDate}</Text>
-                                            <Text style={styles.name}>Require until: {hostWaitingQueue.requireUntilDate}</Text>
-                                        </View>
-                                    }
-                                </Card>
+                                <TouchableOpacity onPress={() => this.setUser(hostWaitingQueue)} style={{ left: 10 }}>
+                                    <Card>
+                                        {
+                                            <View key={i} style={styles.user}>
+                                                <Text style={styles.name}>Requested by: {hostWaitingQueue.bookedBy}</Text>
+                                                <Text style={styles.name}>Require to: {hostWaitingQueue.requireToDate}</Text>
+                                                <Text style={styles.name}>Require until: {hostWaitingQueue.requireUntilDate}</Text>
+                                                <View style={styles.input}>
+                                                    <VerifyPopUp dialogVisible={this.state.dialogVisibleAnswerParking} closePopUp={this.closePopUp} subject='Approve or Decline the Request' topTitle='Let the driver know your answer' />
+                                                </View>
+                                            </View>
+                                        }
+                                    </Card>
+                                </TouchableOpacity>
                             )
                         })}
                     </ScrollView>
